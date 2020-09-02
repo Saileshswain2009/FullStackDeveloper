577cc4a1745385b39fbea60780ffee70f26b1399
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:07:29 +0530

    37 commit message

---
+++ MultipleDeligateExample2/MultipleDeligateExample2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MultipleDeligateExample2
+{
+    public delegate void rita(int z);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rita obj = new rita(M.muti);
+            obj += M.div;
+            obj(200);
+        }
+    }
+    class M
+    {
+        public static void muti(int a1)
+        {
+            Console.WriteLine("Multiplication is: {0}", a1*2);
+        }
+        public static void div(int a2)
+        {
+            Console.WriteLine("Division is: {0}", a2 / 10);
+        }
+    }
+}
