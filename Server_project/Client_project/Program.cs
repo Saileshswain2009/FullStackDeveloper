7fe78d2ba76834e9cdc7ecd140141d9c5527eeea
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 01:02:40 +0530

    success

---
+++ Server_project/Client_project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client_project
+{
+    class ClientOpera
+    {
+        static Socket sck;
+        public static void Main(string[] args)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Main(args);
+            }
+            Console.WriteLine("Enter text");
+            String str = Console.ReadLine();
+            byte[] data = Encoding.ASCII.GetBytes(str);
+            sck.Send(data);
+            Console.Write("Data send!\r\n");
+            Console.Write("Press any key to continue...");
+            Console.Read();
+            sck.Close();
+        }
+    }
+}
---
+++ Server_project/Server_project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server_project
+{
+    class ServerOperation
+    {
+        static byte[] Buffer { get; set; }
+        static Socket sck;
+        public static void Main(string[] args)
+        {
+            ServerOperation so = new ServerOperation();
+            so.ServerConn();
+        }
+        public void ServerConn()
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(0, 1234));//This socket will bind our socket to a certain end point.
+            sck.Listen(100);
+            Console.WriteLine("Server is Connected");
+            Socket accepted = sck.Accept();//It going to connect a socket which are in waiting state.
+            Buffer = new byte[accepted.SendBufferSize];
+            int bytesRead = accepted.Receive(Buffer);//wait ,receive and transfer the buffer to Buffer.
+            byte[] formatted = new byte[bytesRead];
+            for (int i = 0; i < bytesRead; i++)
+            {
+                formatted[i] = Buffer[i];
+            }
+            string strData = Encoding.ASCII.GetString(formatted);
+            Console.Write(strData + "\r\n");
+            Console.Read();
+            sck.Close();
+            accepted.Close();
+        }
+    }
+}
