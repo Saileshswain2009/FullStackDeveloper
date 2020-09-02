07a4dfdbc94a6edfe958b1875549e2feb415b6bd
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:24:42 +0530

    success

---
+++ UsingRunMethod/UsingRunMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UsingRunMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name="Main Thread";
+
+            Task RMethod = Task.Run(() =>
+                                      Console.WriteLine("Hello from Rmethod"));
+    
+            Console.WriteLine("Hello from Thread: {0}", Thread.CurrentThread.Name);
+            RMethod.Wait();//to wait the Rmethod thread after execution of hello from thread we must use threadName.wait() after Hello from Thread.
+        }
+    }
+}
