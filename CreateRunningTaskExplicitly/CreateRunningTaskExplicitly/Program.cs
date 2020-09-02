9c709a5ab0ab4911f40051eddada66169d9a3fb4
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:17:30 +0530

    success

---
+++ CreateRunningTaskExplicitly/CreateRunningTaskExplicitly/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CreateRunningTaskExplicitly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Sailesh";
+            Task taskA = new Task(() => Console.WriteLine("Hello to Task World")); // Create a task and supply a user delegate by using a lambda expression.
+            taskA.Start();
+            Console.WriteLine("Hello from {0}", Thread.CurrentThread.Name);
+            taskA.Wait(10);
+        }
+    }
+}
