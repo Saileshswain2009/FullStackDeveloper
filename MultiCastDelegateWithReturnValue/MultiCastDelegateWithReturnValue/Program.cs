5214bec3f8dfaccee5dae12e96ccc4cfcd052ec2
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:04:39 +0530

    36 commit message

---
+++ MultiCastDelegateWithReturnValue/MultiCastDelegateWithReturnValue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MultiCastDelegateWithReturnValue
+{
+    public delegate int MyDelegate(); //declaring a delegate
+
+    class MultDelArg
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del1 = ClassA.MethodA;
+            MyDelegate del2 = ClassB.MethodB;
+
+            MyDelegate del = del1 + del2;
+            Console.WriteLine(del());// returns 200
+        }
+    }
+
+    class ClassA
+    {
+        public static int MethodA()
+        {
+            return 100;
+        }
+    }
+
+    class ClassB
+    {
+        public static int MethodB()
+        {
+            return 200;
+        }
+    }
+
+}
