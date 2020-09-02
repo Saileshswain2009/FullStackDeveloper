e9dead741e373ae2822e7ef83f249727462d02bb
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:29:47 +0530

    18 commit message

---
+++ LinqToXMLExample1/LinqToXMLExample1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace LinqToXMLExample1
+{
+    class ExampleOfXML
+    {
+        static void Main(string[] args)
+        {
+            string myXML=@"<Departments>
+                            <Department>Account</Department>
+                            <Department>Sales</Department>
+                            <Department>Pre-Sales</Department>
+                            <Department>Marketing</Department>
+                            </Departments>
+            XDocument xdoc=new XDocument();
+            xdoc=XDocument.Parse(myXML)
+            var result=xdoc.Element("Departments").Departments();
+            foreach (XElement item in result)
+            {
+                Console.WriteLine("Department Name-" + item.Value);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
