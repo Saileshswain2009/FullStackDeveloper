8cf69766a0d64a65dfc55351d01880462a3c7c05
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:35:51 +0530

    23 commit message

---
+++ ThreadExample1/ThreadExample1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = new Task(() =>
+              {
+                  for (int i = 0; i < 5; i++)
+                  {
+                      var threadId = Thread.CurrentThread.ManagedThreadId;
+                      Console.WriteLine("Task Loop Current Thread Id:" + threadId);
+                  }
+              });
+            t.Start();
+
+            for(int i=0;i<5;i++)
+            {
+                var threadId = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine("Main Loop current thread Id:" + threadId);
+            }
+            t.Wait();
+            Console.WriteLine("Press enter terminate the process!");
+            Console.ReadLine();
+        }
+    }
+}
