311012b4c51f59322f87617a652113932161df0c
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:39:33 +0530

    success

---
+++ FileInputOutput_project/FileInputOutput_project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileInputOutput_project
+{
+    class FileInOut
+    {
+        static void Main(string[] args)
+        {
+            WriteToFile wr = new WriteToFile();
+            wr.Data();
+            Console.ReadKey();
+        }
+        public void Data()
+        {
+            StreamWriter sw = new StreamWriter("F://bizruntime.txt");//StreamWriter is used for text writing on a particular place.
+            Console.WriteLine("Enter the Text that you want to write on File");
+            string str = Console.ReadLine();
+            sw.WriteLine();//To write a aline in the buffer.
+            sw.Flush();// To write in output stream 
+            sw.Close();
+        }
+    }
+}
