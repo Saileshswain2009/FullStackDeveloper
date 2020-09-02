d0e1e5d0b6a454c42ac18a32df07736e4e0ed19c
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:51:26 +0530

    31 commit message

---
+++ EventBasic1/EventBasic1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Security;
+
+namespace EventBasic1
+{
+    public class SimpleEvent
+    {
+        public SimpleEvent(string text) { Text = text; }
+        public string Text { get; }
+    }
+    class Publisher
+    {
+        public delegate void SampleEventHandler(object sender, SimpleEvent e);
+
+        public event SampleEventHandler SimpleEvent;
+
+        // Wrap the event in a protected virtual method
+        // to enable derived classes to raise the event.
+        protected virtual void RaiseSampleEvent()
+        {
+            SimpleEvent?.Invoke(this, new SimpleEvent("Welcome"));
+        }
+    }
+}
