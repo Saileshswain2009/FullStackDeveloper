4e1c4fc614e635191df605bd14c896bf2d2acbcb
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:20:50 +0530

    14 commit message

---
+++ LambdaExpressionExample1/LambdaExpressionExample1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LambdaExpressionExample1
+{
+    class Program
+    {
+        delegate int del(int i); 
+        static void Main(string[] args)
+        {
+            del myDelegate = y => y * y;
+            int j = myDelegate(5);
+            Console.WriteLine("The result is:{0}",j);
+            Console.ReadLine();
+        }
+    }
+}
