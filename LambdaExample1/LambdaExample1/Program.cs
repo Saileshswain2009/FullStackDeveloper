1bb0c346d6fd55ccb2a390120cdb81ccb44e08dc
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:18:47 +0530

    13 commit message

---
+++ LambdaExample1/LambdaExample1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+            double avg = num.Where(x => x % 2 == 1).Average();
+            Console.WriteLine("The average value of all odd number in this fibonacci series is: {0}",avg);
+            Console.ReadLine();
+        }
+    }
+}
