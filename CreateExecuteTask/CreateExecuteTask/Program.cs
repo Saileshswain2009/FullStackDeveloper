3aee8493d9756158b78a356bae98d0f1873fbfd1
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:15:34 +0530

    success

---
+++ CreateExecuteTask/CreateExecuteTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CreateExecuteTask
+{
+    class ExecuteTask
+    {
+        public static  async Task Main(string[] args)
+        {
+            await Task.Run(() =>
+                        {
+                            int count = 0;
+                            for (count = 0; count <= 10; count++)
+                            { }
+                            Console.WriteLine("Finished {0} loop iterations", count);
+                        });
+        }
+    }
+}
