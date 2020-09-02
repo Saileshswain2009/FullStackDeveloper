3b2e6919c5da698b8794a4f937f1c644518e0b2e
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:50:41 +0530

    success

---
+++ StreamWriterUsingStreamBuilder_project/StreamWriterUsingStreamBuilder_project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StreamWriterUsingStreamBuilder_project
+{
+    class StreamWriter
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter();
+            WriteHello(sw);
+            Console.WriteLine(sb);
+        }
+        static void WriteHello(TextWriter tw)
+        {
+            tw.Write("Hello SAILESH,string I/O!");
+        }
+    }
+}
