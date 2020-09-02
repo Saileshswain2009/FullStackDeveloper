2349a1850e20a75de6d79615f7181d8ac0ffd211
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:18:54 +0530

    success

---
+++ TaskFactoryStartNewMethod/TaskFactoryStartNewMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskFactoryStartNewMethod
+{
+     class TaskFactory
+    {
+         static void Main(string[] args)
+        {
+            Task t = Task.Factory.StartNew(() =>
+                                              {
+                                                  int ctr = 0;
+                                                  for(ctr=0;ctr<10;ctr++)
+                                                  {}
+                                                  Console.WriteLine("Finised Loop iteration is: {0}", ctr);
+                                              });
+            t.Wait();
+        }
+    }
+}
