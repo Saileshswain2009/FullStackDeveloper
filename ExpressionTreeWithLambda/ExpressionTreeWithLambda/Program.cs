5df1a7659e95b6987db881f11b28bbc7cb2a03bc
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:15:40 +0530

    12 commit message

---
+++ ExpressionTreeWithLambda/ExpressionTreeWithLambda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpressionTreeWithLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pin = { 3, 8, 4, 6, 1, 7, 9, 2, 4, 8 };
+            foreach (int i in pin.Where(x =>
+             {
+                 if (x <= 3)
+                     return true;
+                 else if (x >= 7)
+                     return true;
+                 return false;
+             }
+            )
+            )
+                Console.WriteLine(i);
+        }
+    }
+}
