3a1215660366e8e39eaf6e1d1d2bf5acbce37c79
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 22:51:17 +0530

    fivth commit

---
+++ DelegatesExample1/DelegatesExample1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Linq;
+
+namespace DelegatesExample1
+{
+    class StudentExtension
+    {
+        public static Student[] where(Student[] stdArray, FindStudent del)
+        {
+            int i = 0;
+            Student[] result = new Student[10];
+            foreach (Student std in stdArray)
+                if (del(std))
+                {
+                    result[i] = std;
+                    i++;
+                }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 } ,
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 } ,
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  } ,
+        };
+
+            Student[] students = StudentExtension.where(studentArray, delegate (Student std) {
+                return std.Age > 12 && std.Age < 20;
+            });
+        }
+    }
+}
+}
