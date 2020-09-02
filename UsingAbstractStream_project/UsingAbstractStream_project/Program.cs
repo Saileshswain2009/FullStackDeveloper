8813da434c1f713ecd4bf76b4f4a617c74e84bef
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:51:27 +0530

    success

---
+++ UsingAbstractStream_project/UsingAbstractStream_project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace UsingAbstractStream_project
+{
+    class AbstractStream
+    {
+        static void Main(string[] args)
+        {
+            Stream s = new FileStream("foo.txt", FileMode.Create);//Stream:-provide a generic view of sequence of btye.This is a abstract class.
+            s.WriteByte(77);
+            s.WriteByte(127);
+            s.Close();
+        }
+    }
+}
+//For writing bytes to the current file.
