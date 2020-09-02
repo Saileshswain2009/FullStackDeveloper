129fd5eb00f03d6690ec7f24521632cb39a0d75b
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 01:01:48 +0530

    success

---
+++ MultiConnSocket_project/MultiConnSocket_project/Listener.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MultiConnSocket_project
+{
+    class Listener
+    {
+        Socket s;
+        public bool Listening
+        {
+            get;
+            private set;
+        }
+        public int port
+        {
+            get;
+            private set;
+        }
+        public Listener(int port)//constructor
+        {
+            port = port;
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Start()
+        {
+            if (Listening)
+                return;
+            s.Bind(new IPEndPoint(0, port));
+            s.Listen(0);
+            s.BeginAccept(callback, null);
+            Listening = true;
+
+        }
+        public void Stop()
+        {
+            if (!Listening)
+                return;
+            s.Close();
+            s.Dispose();//disconnect resaon
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        }
+        void callback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket s = this.s.EndAccept(ar);
+                if(SocketAccepted != null)
+                {
+                    SocketAccepted(s);
+                }
+                this.s.BeginAccept(callback, null);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public delegate void SocketAccetedHandler(Socket e);
+        public event SocketAccetedHandler SocketAccepted;
+    }
+}
---
+++ MultiConnSocket_project/MultiConnSocket_project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace MultiConnSocket_project
+{
+    class ServerOperation
+    {
+        static Listener l;
+        static List<Socket> sockets;
+        public static void Main(string[] args)
+        {
+            ServerOperation so = new ServerOperation();
+            so.ListenerOperation();
+        }
+         public void ListenerOperation()
+        {
+            l = new Listener(8);
+            l.SocketAccepted += new Listener.SocketAccetedHandler(l_SocketAccepted);
+            l.Start();
+            sockets = new List<Socket>();
+            Console.Read();
+        }
+        static void l_SocketAccepted(System.Net.Sockets.Socket e)
+        {
+            Console.WriteLine("New Connection: {0}\n{1}\n=========", e.RemoteEndPoint, DateTime.Now);//I.p Address of the Endpoint & current time to the console.
+            sockets.Add(e);
+        }
+    }
+}
