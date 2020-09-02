bc053bb74651e1ac895976862b9afc1983070171
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 01:03:24 +0530

    success

---
+++ ServerOpera_project/ClientOpera_project/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientOpera_project
+{
+    class Client
+    {
+        public static void Main(string[] args)
+        {
+            Client c = new Client();
+            c.CliMessage();
+        }
+        public void CliMessage()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1994);
+            s.Connect(endPoint);
+            Console.Write("Enter Message");
+            String str = Console.ReadLine();
+            byte[] strBuffer = Encoding.Default.GetBytes(str);
+            s.Send(strBuffer, 0, strBuffer.Length, 0);
+            byte[] buffer = new byte[255];
+            int rec = s.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Received: {0}", Encoding.Default.GetString(buffer));
+            Console.Read();
+        }
+    }
+}
---
+++ ServerOpera_project/ServerOpera_project/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerOpera_project
+{
+    class Server
+    {
+        public static void Main(string[] args)
+        {
+            Server se = new Server();
+            se.SocMessage();
+        }
+        public void SocMessage()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Bind(new IPEndPoint(0, 1994));
+            s.Listen(0);
+            Socket acc = s.Accept();
+            byte[] buffer = Encoding.Default.GetBytes("Hello client");
+            acc.Send(buffer, 0, buffer.Length, 0);
+            buffer = new byte[255];
+            int rec = acc.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Received: {0}", Encoding.Default.GetString(buffer));
+            s.Close();
+            acc.Close();
+            Console.Read();
+        }
+    }
+}
