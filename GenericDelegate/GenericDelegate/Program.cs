e3e2505b1ff027cfca39b1f3ae71ef8ba1e8f510
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:53:57 +0530

    33 commit message

---
+++ GenericDelegate/GenericDelegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericDelegate
+{
+    public delegate T add<T>(T param1, T param2);
+    class GDelegate
+    {
+        static void Main(string[] args)
+        {
+            add<int> M1 = sum;
+            Console.WriteLine("Addition is:"+M1(20, 30));
+            add<int> M2 = diff;
+            Console.WriteLine("Substraction is:"+M2(50, 20));
+        }
+
+        public static int sum(int val1,int val2)
+        {
+            return val1 + val2;
+        }
+        public static int diff(int val1,int val2)
+        {
+            return val1 - val2;
+        }
+
+    }
+}
