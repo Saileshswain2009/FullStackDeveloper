f3a9725d0bd4cba0e58b472323a05b7b39a8b39a
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:03:45 +0530

    seveth commit message

---
+++ LinqExample1/LinqExample1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = { "Sailesh", "Amit", "Bikash", "Javed", "Malika","Swagat","Sandhaya" };
+            var nQuery = from str1 in str
+                        where str1[2]<0
+                        select str1;
+            foreach(var i in nQuery)
+            {
+                Console.WriteLine(i + ",");
+            }
+        }
+    }
+}
