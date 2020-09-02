1865c7d66b610bfcf8a29627195a2c8378de0fa1
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:42:58 +0530

    26 commit message

---
+++ DelegateAnonymousMethod/DelegateAnonymousMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DelegateAnonymousMethod
+{
+    public delegate void dName();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dName d1 = delegate
+                        {
+                            Console.WriteLine("Anonymous method");
+                        };
+            d1();
+            Console.ReadLine();
+        }
+    }
+}
