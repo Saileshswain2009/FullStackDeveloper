9483ff9ba2d4e13dbaab9c4893df6320b2062d77
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:59:45 +0530

    35 commit message

---
+++ MulticastDelegateExample/MulticastDelegateExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MulticastDelegateExample
+{
+    public delegate void mDelegate(int x);
+    class MultiDelegate
+    {
+        static void Main(string[] args)
+        {
+            mDelegate m1 = new mDelegate(TwoDeligate.Add);
+            m1 += TwoDeligate.Square;//Calling multiple method(+=) 
+            m1(5);
+            m1(6);
+        }
+    }
+    class TwoDeligate
+    {
+        public static void Add(int a)
+        {
+            Console.WriteLine("Addition is:{0}",a+20);
+        }
+        public static void Square(int b)
+        {
+            Console.WriteLine("Square is:{0}", b*5);
+        }
+    }
+}
