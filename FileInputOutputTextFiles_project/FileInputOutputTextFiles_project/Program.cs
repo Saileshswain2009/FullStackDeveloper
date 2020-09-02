0c21d57f5509ab4a452b8dca5ba7b054e7b21c35
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:41:39 +0530

    success

---
+++ FileInputOutputTextFiles_project/FileInputOutputTextFiles_project/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileInputOutputTextFiles_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("F:/file1.txt"))
+                {
+                    string str = "Hello to BizRunTime";
+                    sw.Write(str);
+                }
+                using(StreamReader sr=new StreamReader("F:/file1.txt"))
+                {
+                    string str;
+                    while((str=sr.ReadLine())!= null)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
