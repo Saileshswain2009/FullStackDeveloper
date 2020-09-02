a5da188ca6234ec97ade92dca774768aa26baabf
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:52:08 +0530

    success

---
+++ UsingStreamReaderAndStreamWriter_project/UsingStreamReaderAndStreamWriter_project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace UsingStreamReaderAndStreamWriter_project
+{
+    class StreamWriterOperation
+    {
+        static void Main(string[] args)
+        {
+            Stream fs = new FileStream("zoo.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs, Encoding.ASCII);
+            sw.Write("Hello Brother");
+            sw.Close();
+        }
+    }
+}
