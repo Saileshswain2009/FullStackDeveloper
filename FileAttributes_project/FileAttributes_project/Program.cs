f2ca107322d627e92c25965c8d14bd2f472275d3
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:53:20 +0530

    success

---
+++ FileAttributes_project/FileAttributes_project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileAttributes_project
+{
+    class FileAttributeOperations
+    {
+        static void Main(string[] args)
+        {
+            FileAttributes attributes = File.GetAttributes("F:/temp/MyText.txt");
+            if((attributes & FileAttributes.ReadOnly)==FileAttributes.ReadOnly)
+            {
+                Console.WriteLine("read-only File");
+            }
+            else
+            {
+                Console.WriteLine("Not read-only Files");
+            }
+        }
+    }
+}
