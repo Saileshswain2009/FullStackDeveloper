99754eb6ececd1fccb895c8e79e3458a128fd5cc
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:52:47 +0530

    32 commit message

---
+++ ExceptionInDelegate/ExceptionInDelegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExceptionInDelegate
+{
+    public class Procedure
+    {
+        public static void One()
+        {
+            Console.WriteLine("One Display:");
+            throw new Exception("Error");
+        }
+        public static void Two()
+        {
+            Console.WriteLine("Two Display:");
+        }
+    }
+    class Operation
+    {
+        delegate void oDelegate();
+        static void Main(string[] args)
+        {
+            oDelegate obj = new oDelegate(Procedure.One);
+            obj += Procedure.Two;
+            Delegate[] del = obj.GetInvocationList();
+            foreach(oDelegate i in del)
+            {
+                try
+                {
+                    i();
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Got Error");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
