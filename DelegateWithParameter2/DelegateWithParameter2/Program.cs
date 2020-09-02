16ece79a65e69a322e1dcf3b295a851c5cd269f8
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:49:41 +0530

    29 commit message

---
+++ DelegateWithParameter2/DelegateWithParameter2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DelegateWithParameter2
+{
+    public delegate void sandeep(string str);
+    class DelParam
+    {
+        static void Main(string[] args)
+        {
+            sandeep obj = new sandeep(A.M1);
+            Z(obj);
+
+            obj = B.M2;
+            Z(obj);
+
+            obj = (string str) => Console.WriteLine("Called lambda expression:" + str);
+            Z(obj);
+        }
+        static void Z(sandeep obj)//Passing delegate parameter
+        {
+            obj("Hello Sandeep");
+        }
+    }
+    class A
+    {
+        public static void M1(string str)
+        {
+            Console.WriteLine("Welcome to M1 method:", str);
+        }
+    }
+    class B
+    {
+        public static void M2(string str)
+        {
+            Console.WriteLine("Welcome to M2 method:", str);
+        }
+    }
+}
