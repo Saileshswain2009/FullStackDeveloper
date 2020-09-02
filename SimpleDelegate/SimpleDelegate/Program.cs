344f10c59c7b35075f7463ef7ad82806e128e93c
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:09:11 +0530

    38 commit message

---
+++ SimpleDelegate/SimpleDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace SimpleDelegate1
+{
+    public delegate int operation1(int m1, int m2);
+
+    class DelegateOperation
+    {
+        static int diff(int x1, int x2)
+        {
+            return x1 - x2;
+        }
+        public static void Main(string[] args)
+        {
+            operation1 obj = new operation1(DelegateOperation.diff);
+            Console.WriteLine("The Difference is:{0}", obj(100, 20));
+            Console.ReadLine();
+        }
+    }
+}
