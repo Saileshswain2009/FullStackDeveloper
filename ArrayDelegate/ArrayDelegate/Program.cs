8a86bef6f25451af7aed484dcca8ce85ca6f1f88
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:39:37 +0530

    25 commit message

---
+++ ArrayDelegate/ArrayDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArrayDelegate
+{
+    public class Execute
+    {
+        public static void Add(int x,int y)
+        {
+            Console.WriteLine("Addition={0}", x + y);
+        }
+        public static void Multiply(int x,int y)
+        {
+            Console.WriteLine("Multiplication={0}", x * y);
+        }
+    }
+    class Test
+    {
+        public delegate void Math(int a, int b);//If method having no return type then return type of delegate is void.
+        static void Main(string[] args)
+        {
+            Math[] m1 = {
+                            new Math(Execute.Add),
+                            new Math(Execute.Multiply)
+                        };
+            for(int i=0;i<m1.Length;i++)
+            {
+                m1[i](2, 4);
+                m1[i](10, 20);
+            }
+            Console.ReadLine();
+        }
+    }
+}
