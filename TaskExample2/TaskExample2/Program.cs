e5f1d0d47dcacb590bda67f1bba3e23f48ab194b
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:31:51 +0530

    20 commit message

---
+++ TaskExample2/TaskExample2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>//Creating a task object and pass anonymous method to task constructor parameter as work to do within the task
+            {
+                return 32;
+            });
+            Console.WriteLine(t.Result);
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to terminate!");
+            Console.ReadLine();
+        }
+    }
+}
