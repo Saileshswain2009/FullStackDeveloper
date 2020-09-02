66f09b24692e00dadd71f3ff81b354bd2d092c6c
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:27:01 +0530

    success

---
+++ WaitOneOrMoreTask/WaitOneOrMoreTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaitOneOrMoreTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = Task.Run(() => Thread.Sleep(5000));
+            Console.WriteLine("Staus of t1 thread is: {0} ", t1.Status);
+            try
+            {
+                t1.Wait();
+                Console.WriteLine("Status of thread inside try block is: {0}", t1.Status);
+            }
+            catch(AggregateException)
+            {
+                Console.WriteLine("Exception in Task t1");
+            }
+        }
+    }
+}
