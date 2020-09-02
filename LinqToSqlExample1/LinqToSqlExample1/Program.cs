d24c8a0a2210bb3e6f363c849f31f5b7a277d309
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:28:13 +0530

    17 commit message

---
+++ LinqToSqlExample1/LinqToSqlExample1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqToSqlExample1
+{
+    class LinqToSQLCRUD
+    {
+        static void Main(string[] args)
+        {
+            string connectString = System.Configuration.ConfigurationManager.ConnectionStrings["LinqToSQLDBConnectionString"].toString();
+            LinqToSQLDataContext db = new LinqToSQLDataContext(connectString);
+
+            Employee newEmployee = new Employee();
+            newEmployee.Name = "Sailesh";
+            newEmployee.Email = "saileshswain2009@gmail.com";
+            newEmployee.ContantN.o = "8658821545";
+            newEmployee.DepartmentId = "3";
+            newEmployee.Address = "Sailesh-INDIA";
+
+            db.Employees.InsertOnSubmit(newEmployee);
+
+            db.SubmitChanges();
+
+            Employee insertedEmployee = db.Employees.FirstOrDefault(e => e.Name.Equals("Sailesh"));
+
+            Console.WriteLine("Employee Id = {0} , Name = {1}, Email = {2}, ContactNo = {3}, Address = {4}",
+                          insertedEmployee.EmployeeId, insertedEmployee.Name, insertedEmployee.Email,
+                          insertedEmployee.ContactNo, insertedEmployee.Address);
+
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
