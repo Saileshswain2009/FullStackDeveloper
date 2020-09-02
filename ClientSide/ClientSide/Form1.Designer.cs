85a8bad6de99f838242b9367335e325e236c3660
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:56:06 +0530

    success

---
+++ ClientSide/ClientSide/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ClientSide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtServerMessage
+            // 
+            this.txtServerMessage.Location = new System.Drawing.Point(57, 45);
+            this.txtServerMessage.Multiline = true;
+            this.txtServerMessage.Name = "txtServerMessage";
+            this.txtServerMessage.Size = new System.Drawing.Size(665, 164);
+            this.txtServerMessage.TabIndex = 0;
+            this.txtServerMessage.TextChanged += new System.EventHandler(this.txtServerMessage_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server Messages";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(57, 228);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(665, 112);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // btnsend
+            // 
+            this.btnsend.Location = new System.Drawing.Point(720, 228);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(68, 24);
+            this.btnsend.TabIndex = 3;
+            this.btnsend.Text = "Send";
+            this.btnsend.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message send to server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 354);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServerMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServerMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnsend;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ClientSide/ClientSide/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ClientSide
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client = null;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TcpClient("127.0.0.1", 8888);//To get the server and read the msg.
+            NetworkStream ns = client.GetStream();//To get the server stream(NetworkStream)
+            StreamReader sr = new StreamReader(ns);
+
+            txtServerMessage.Text = "Server>>" + sr.ReadLine();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if(txtMessage.Text!="")
+            {
+                NetworkStream ns = client.GetStream();
+                StreamWriter sw = new StreamWriter(ns);
+                sw.WriteLine(txtMessage.Text);
+                sw.Flush();
+                sw.Close();
+                ns.Close();
+            }
+        }
+
+        private void txtServerMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
