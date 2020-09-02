c3ddcfe8f0e46ad8f2c66c21737fa12fb929d50f
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:50:00 +0530

    success

---
+++ OperationOnDirectoriesAndFiles_project/OperationOnDirectoriesAndFiles_project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace OperationOnDirectoriesAndFiles_project
+{
+    class FileStreamOperation
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Stream s = File.OpenRead(args[0]);//OpenRead:-open an existing file for reading.
+                StreamReader sr = new StreamReader(s);
+                Console.WriteLine(sr.ReadLine());
+                sr.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
