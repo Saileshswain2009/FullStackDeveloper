981d474a6b25e094a4905418f81d7ab12c17539f
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:32:50 +0530

    21 commit message

---
+++ TaskExample3/TaskExample3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> obj = Task.Run(() =>
+            {
+                return 32;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            });
+            obj.ContinueWith((i) =>
+            {
+                
+                Console.WriteLine(i.Result);
+ 
+            });
+            Console.WriteLine("Please Enter to terminate!");
+            Console.ReadLine();
+        }
+    }
+}
