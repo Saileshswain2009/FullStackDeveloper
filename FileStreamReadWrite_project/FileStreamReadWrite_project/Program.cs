54e117e96b033618f5b2d60edba6292799ac4d9c
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 00:49:19 +0530

    success

---
+++ FileStreamReadWrite_project/FileStreamReadWrite_project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamReadWrite_project
+{
+    class FileReadWrite
+    {
+        static void Main(string[] args)
+        {
+            string path = @"F:\temp\Mytext.txt";
+            if(!File.Exists(path))
+            {
+                using(StreamWriter sw=File.CreateText(path))
+                {
+                    sw.WriteLine("SAILESH ");
+                    sw.WriteLine("KUMAR ");
+                    sw.WriteLine("SWAIN");
+                }
+            }
+            using(StreamReader sr=File.OpenText(path))
+            {
+                string s;
+                while((s=sr.ReadLine())!=null)
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
