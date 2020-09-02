fe899a920b71b9cd4c3918c9d0a8e6f829aaaab6
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:50:40 +0530

    30 commit message

---
+++ EventBasic/EventBasic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EventBasic
+{
+    delegate void tDelegate();
+    class SimpleEvent
+    {
+        public static event tDelegate add;//Declare
+        static void Main(string[] args)
+        {
+            add += new tDelegate(USA);//raise
+            add += new tDelegate(INDIA);//consume
+            add += new tDelegate(RUSSIA);//register with the event
+            add.Invoke();
+        }
+        static void USA()//Event handler
+        {
+            Console.WriteLine("Welcome to USA");
+        }
+        static void INDIA()
+        {
+            Console.WriteLine("Welcome to INDIA");
+        }
+        static void RUSSIA()
+        {
+            Console.WriteLine("Welcome to RUSSIA");
+        }
+    }
+}
