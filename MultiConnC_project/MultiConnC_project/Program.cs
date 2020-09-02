8a9eadb6e36f67b815b7d882681b640fb88a5fba
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 01:01:03 +0530

    success

---
+++ MultiConnC_project/MultiConnC_project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MultiConnC_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect("127.0.0.1", 8);
+            s.Close();
+            s.Dispose();
+        }
+    }
+}
