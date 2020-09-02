273bb0e36da3e8f267f3f2d0fffe0cdb7c421b09
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-02 23:34:34 +0530

    22 commit message

---
+++ TaskExample4/TaskExample4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 32;
+            });
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Cancalled");
+            },TaskContinuationOptions.OnlyOnFaulted);
+            var completedTask = t.ContinueWith((i) =>
+              {
+                  Console.WriteLine(i.Result);
+                  Console.WriteLine("Completed");
+              }, TaskContinuationOptions.OnlyOnRanToCompletion);
+            Console.WriteLine("Press Enter to terminate");
+            Console.ReadLine();
+        }
+    }
+}
