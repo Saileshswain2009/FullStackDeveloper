76899aefff8b6d5f3bc925570e3678b7a9208572
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:34:04 +0530

    Success

---
+++ AbstractionExample1/AbstractionExample1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AbstractionExample1
+{
+    abstract class Animal
+    {
+        public abstract void animalSound();
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+    class Pig:Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig say wee wee");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig obj = new Pig();
+            obj.animalSound();
+            obj.sleep();
+        }
+    }
+}
