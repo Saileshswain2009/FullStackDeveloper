5362a0f20bfd57ea82515a28e93c2a0b56525af2
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:37:53 +0530

    Success

---
+++ InheritanceExample6/InheritanceExample6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace InheritanceExample6
+{
+    class Rectangle
+    {
+        protected double length;
+        protected double width;
+
+        public Rectangle(double l,double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double Area()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width:  {0}", width);
+            Console.WriteLine("Area:   {0}", Area());
+        }
+    }
+    class Table:Rectangle
+    {
+        private double cost;
+        public Table(double l,double w):base(l,w)
+        {
+
+        }
+        public double GetCost()
+        {
+            double cost;
+            cost = Area() * 70;
+            return cost;
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine("Cost: {0}", GetCost());
+        }
+    }
+    class ResultedRectangle
+    {
+        static void Main(string[] args)
+        {
+            Table obj = new Table(1.5,2.5);
+            obj.Display();
+        }
+    }
+}
