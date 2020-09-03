17e1b70328db9473cfe56facb5bc185ec145e822
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 18:51:53 +0530

    Success

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = @"Data Source=(local);Initial Catalog=master;Integrated Security=True";
+            SQLConnection conn = new SQLConnection(ConnectionString);
+            try
+            {
+                conn.Open();
+            }
+            
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Connection success");
+        }
+    }
+}
