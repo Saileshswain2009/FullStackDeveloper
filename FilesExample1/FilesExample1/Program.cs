ba780fa6b6125fc713fab637d4ed5a35842f6dd2
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:36:31 +0530

    Success

---
+++ FilesExample1/FilesExample1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FilesExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hello to c# World";
+            File.WriteAllText("fileName.txt", str1);
+
+            string str2 = File.ReadAllText("fileName.txt");
+            Console.WriteLine(str2);
+        }
+    }
+}
