d89b9c3e7b737b219529f1c4a28a346f6b53c1ef
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:41:10 +0530

    Success

---
+++ PolymorphismExample2/PolymorphismExample2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PolymorphismExample2
+{
+    class Bird
+    {
+        public void Color()
+        {
+            Console.WriteLine("Blue in color");
+        }
+    }
+    class Nightangle : Bird 
+    {
+        public void Color()
+        {
+            Console.WriteLine("Having different colors");
+        }
+    }
+    class Parrot:Bird
+    {
+        public void Color()
+        {
+            Console.WriteLine("Green in color");
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Bird obj1 = new Bird();
+            Bird obj2 = new Nightangle();
+            Bird obj3 = new Parrot();
+            obj1.Color();
+            obj1.Color();
+            obj1.Color();
+        }
+    }
+}
