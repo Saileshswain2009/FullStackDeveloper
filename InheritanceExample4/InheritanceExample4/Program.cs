dcb51a243cb0c5ede5e913c3d71abfde28714572
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:30:00 +0530

    Success

---
+++ InheritanceExample4/InheritanceExample4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InheritanceExample4
+{
+    class Shape
+    {
+        protected int width, height;
+        public void SWidth(int w)
+        {
+            width = w;
+        }
+        public void SHeight(int h)
+        {
+            height = h;
+        }
+    }
+    class Rectangle:Shape
+    {
+        public int Area()
+        {
+            return (width * height);
+        }
+    }
+    class Geomatry
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.SWidth(10);
+            rect.SHeight(20);
+            Console.WriteLine("Total area is {0}", rect.Area());
+        }
+    }
+}
