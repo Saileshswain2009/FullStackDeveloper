4218b8b23305dfbf681070e0f11f8ce520de10e9
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:43:24 +0530

    Success

---
+++ PolymorphsimExample3/PolymorphsimExample3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PolymorphsimExample3
+{
+    class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+    class Pig:Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says : wee wee");
+        }
+    }
+    class Dog:Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The dog says : woo woo");
+        }
+    }
+    class Pet
+    {
+        static void Main(string[] args)
+        {
+            Animal obj1 = new Animal();
+            Animal obj2 = new Pig();
+            Animal obj3 = new Dog();//Here we share the name base class name but we use vitual in base class method and overide in all derived class method.
+            obj1.animalSound();
+            obj2.animalSound();
+            obj3.animalSound();
+        }
+    }
+}
