e1ac0c96b18b59292a00dd602959eab14ed0b6f0
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:38:31 +0530

    Success

---
+++ InterfaceExample1/InterfaceExample1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InterfaceExample1
+{
+    interface IAnimal
+    {
+        void animalSound();
+    }
+    class Dog:IAnimal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("Sounds like woo woo");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog obj = new Dog();
+            obj.animalSound();
+        }
+    }
+}
