96d36231c894682aad16b120fee4d8d9dff45f90
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:34:49 +0530

    Success

---
+++ AbstractionExample2/AbstractionExample2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AbstractionExample2
+{
+    abstract class Mobile
+    {
+        public abstract void Features();
+        public void Design()
+        {
+            Console.WriteLine("I-phone having highest number of features");
+        }
+    }
+    class Gadget:Mobile
+    {
+        public override void Features()
+        {
+            Console.WriteLine("x-series is having good features");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gadget g1 = new Gadget();
+            g1.Design();
+            g1.Features();
+        }
+    }
+}
