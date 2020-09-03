26a10893286c5ded09e6fc251dc5b1ed60bed8f6
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:30:26 +0530

    Success

---
+++ InheritanceExample5/InheritanceExample5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceExample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
