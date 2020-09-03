6d3a3eb0abf453bef6bf6f9eb1f6d1c453bc9906
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:37:11 +0530

    Success

---
+++ FilesExample2/FilesExample2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace FilesExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream M = new FileStream("test.data", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            for (int i = 1; i <= 10; i++)
+            {
+                M.WriteByte((byte)i);
+            }
+
+            M.Position = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(M.ReadByte() + " ");
+            }
+            M.Close();
+        }
+    }
+}
