1ca35c821a75a8e31c7fedd22a2cd55bfe5c3667
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:23:55 +0530

    Success

---
+++ WebApi_project/WebApi_project/ApiController/StudentDataController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Http;
+
+namespace WebApi_project.ApiControllers
+{
+    public class StudentDataController : ApiController
+    {
+        [HttpGet]
+        public int GetData1()
+        {
+            return 10;
+        }
+
+        [HttpGet]
+        public int GetData2(int a,int b)//Get data from Query String
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public string Save(StudentDTO dto)//Get data from body.Parameter always going through query string.
+        {
+            return dto.name + "-testing";
+        }
+    }
+    public class StudentDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ WebApi_project/WebApi_project/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApi_project
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //var cors = new EnableCorsAttribute("https://localhost:44375", "*", "*");
+            //var cors = new EnableCorsAttribute("*", "*", "*");(Globally approach)(controller access)
+            config.EnableCors();
+            // Web API configuration and services
+            config.Formatters.Clear();//Converting XML TO JSON
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                //routeTemplate: "api/{controller}/{action}/{id}",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
