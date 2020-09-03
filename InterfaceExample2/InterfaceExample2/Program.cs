76e776e6dcf76d6e472fcbb1ef4edd0b936bb827
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:31:08 +0530

    Success

---
+++ InterfaceExample2/InterfaceExample2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InterfaceExample2
+{
+    class Book
+    {
+        public string Bname = "Geeta";
+        public void Bdis()
+        {
+            Console.WriteLine("Its very useful to live happy life");
+        }
+    }
+    class chapter:Book
+    {
+        public string Cname = "Intoduction";
+        public void Chap1()
+        {
+            Console.WriteLine("Holy Indian Book");
+        }
+    }
+    class Reading
+    {
+        static void Main(string[] args)
+        {
+            chapter c1 = new chapter();
+            c1.Chap1();//Holy indian book
+            Console.WriteLine(c1.Bname + " " + c1.Cname);//Geeta Intro
+            c1.Bdis();
+        }
+    }
+}
