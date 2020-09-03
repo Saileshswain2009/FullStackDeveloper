7fde110fc04575297be7d59905c3754fb1f10c83
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:35:37 +0530

    Success

---
+++ AbstractionExample3/AbstractionExample3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AbstractionExample3
+{
+    abstract class Dimension
+    {
+        public abstract int Area();
+    }
+    class Rectangle:Dimension
+    {
+        private int len, bre;
+        public Rectangle(int x,int y)
+        {
+            len = x;
+            bre = y;
+        }
+        public override int Area()
+        {
+            int result = len * bre;
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle obj = new Rectangle(10,40);
+            Console.WriteLine("Area of rectangle is {0}", obj.Area());
+        }
+    }
+}
