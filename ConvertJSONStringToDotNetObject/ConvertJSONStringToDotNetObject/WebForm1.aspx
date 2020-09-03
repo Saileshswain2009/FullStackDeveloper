59e0bb1f8a56a57923bb7d3c4c5ef6d3aa908b5d
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 18:35:00 +0530

    Success

---
+++ ConvertJSONStringToDotNetObject/ConvertJSONStringToDotNetObject/WebForm1.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ConvertJSONStringToDotNetObject.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="font-family:Arial">
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ConvertJSONStringToDotNetObject/ConvertJSONStringToDotNetObject/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using System.Web.UI.WebControls;
+
+
+namespace ConvertJSONStringToDotNetObject
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string jsonString = "[{\"firstName\":\"SAILESH\",\"lastName\":\"SWAIN\",\"gender\":\"Male\",\"salary\":50000},{\"firstName\":\"SWETA\",\"lastName\":\"BASTIA\",\"gender\":\"Female\",\"salary\":30000}]";
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            List<Employee> listEmployee=(List<Employee>)javaScriptSerializer.Deserialize(jsonString, typeof(List<Employee>));
+
+            foreach(Employee employee in listEmployee)
+            {
+                Response.Write("FirstName=" + employee.firstName + "<br/>");
+                Response.Write("LastName=" + employee.lastName + "<br/>");
+                Response.Write("Gender=" + employee.gender + "<br/>");
+                Response.Write("Salary=" + employee.salary + "<br/><br/>");
+            }
+        }
+    }
+}
