eefee6c521ee20cbcd7798e1dd62e1fc23d2eca4
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:32:44 +0530

    Success

---
+++ StackOverFlowExample/StackOverFlowExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace StackOverFlowExample
+{
+    class Program
+    {
+        static void Repeat(int x)
+        {
+            
+            try
+            {
+                Console.WriteLine(x);
+                Repeat(++x);
+            }
+            catch(StackOverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Repeat(0);
+            }
+            catch (StackOverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
