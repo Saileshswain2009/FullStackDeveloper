fa35d2ee73e28984539331c0fd5ee6751c45868a
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:40:33 +0530

    Success

---
+++ MultipleInterfaceExample1/MultipleInterfaceExample1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MultipleInterfaceExample1
+{
+    interface IPlace
+    {
+        void Home();
+    }
+    interface ITour
+    {
+        void Sceneries();
+    }
+    class Enjoy:IPlace,ITour
+    {
+        public void Home()
+        {
+            Console.WriteLine("Home is a mini temple");
+        }
+        public void Sceneries()
+        {
+            Console.WriteLine("Once in a year we must go for a tour");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enjoy e1 = new Enjoy();
+            e1.Home();
+            e1.Sceneries();
+        }
+    }
+}
