e823cb06f4255cee9aaec57c255e24728df46e86
Saileshswain2009 <saileshswain2009@gmail.com>
2020-09-03 19:29:26 +0530

    Success

---
+++ InheritanceExample3/InheritanceExample3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InheritanceExample3
+{
+    class Sports
+    {
+        public int player;
+        public void Cricket()
+        {
+            Console.WriteLine("Used to play with 11 players");
+        }
+    }
+    class Games:Sports
+    {
+        public void Football()
+        {
+            Console.WriteLine("Messi is a great player in Football");
+        }
+    }
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Games obj = new Games();
+            obj.Cricket();
+            obj.player = 20;
+            Console.WriteLine(obj.player);
+            obj.Football();
+        }
+    }
+}
